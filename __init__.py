fac7c502c288212d49f599fcffa7b83a0eed98d0
emma-rose22 <57078967+emma-rose22@users.noreply.github.com>
2020-01-28 19:46:25 -0500

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
-from .hello import create_app
+from .app import create_app
 
 APP = create_app()
--- app.py
+++ app.py
@@ -1,15 +1,27 @@
+from decouple import config
 from flask import Flask, render_template
+from .models import DB, User, Tweet
 app = Flask(__name__)
 
 def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
+    app.config['SQLALCEMY_TRACK_MODIFICATIONS'] = False
+    app.config['ENV'] = config('ENV') #change before deploying
+    DB.init_app(app)
     @app.route('/')
     def index():
       return 'Index Page'
 
-    @app.route('/hello')
+    @app.route('/')
     def home():
-      return render_template('base.html', title= 'Hello')
+      users = User.query.all()
+      return render_template('base.html', title='Home', users=users)
 
-    if __name__=="__main__":
-      app.run(debug=True, port = 8080)
+    def reset():
+      '''wont be in final version'''
+      DB.drop_all()
+      DB.create_all()
+      return render_template('base.html', title='DB Reset!', users=[])
+
     return app
